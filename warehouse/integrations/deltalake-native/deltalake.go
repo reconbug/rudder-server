146ff9003a5c4a395c50d061b1f4d91ef15e4b56
Akash Chetty <achetty.iitr@gmail.com>
2023-05-09 19:27:22 +0530

    chore: databricks retries

--- warehouse/integrations/deltalake-native/deltalake.go
+++ warehouse/integrations/deltalake-native/deltalake.go
@@ -134,6 +134,9 @@ type Deltalake struct {
 	LoadTableStrategy      string
 	EnablePartitionPruning bool
 	SlowQueryThreshold     time.Duration
+	Retries                int
+	RetryMaxWait           time.Duration
+	RetryMinWait           time.Duration
 }
 
 func New() *Deltalake {
@@ -147,6 +150,9 @@ func WithConfig(h *Deltalake, config *config.Config) {
 	h.LoadTableStrategy = config.GetString("Warehouse.deltalake.loadTableStrategy", mergeMode)
 	h.EnablePartitionPruning = config.GetBool("Warehouse.deltalake.enablePartitionPruning", true)
 	h.SlowQueryThreshold = config.GetDuration("Warehouse.deltalake.slowQueryThreshold", 5, time.Minute)
+	h.Retries = config.GetInt("Warehouse.deltalake.retries", 10)
+	h.RetryMaxWait = config.GetDuration("Warehouse.deltalake.retryMaxWait", 1, time.Second)
+	h.RetryMinWait = config.GetDuration("Warehouse.deltalake.retryMinWait", 300, time.Second)
 }
 
 // Setup sets up the warehouse
@@ -191,6 +197,7 @@ func (d *Deltalake) connect() (*sqlmiddleware.DB, error) {
 			warehouseutils.GetConfigValue(catalog, d.Warehouse),
 			"",
 		),
+		dbsql.WithRetries(d.Retries, d.RetryMaxWait, d.RetryMinWait),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("creating connector: %w", err)
